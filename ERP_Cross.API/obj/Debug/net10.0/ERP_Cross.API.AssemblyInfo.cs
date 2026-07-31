f8b036e914b9d73bb165d487ef2a675b8e78aa1f
Daniel <silvamartinsdaniel235@gmail.com>
2026-07-31 15:53:28 -0300

    Commit #27.5.1

--- ERP_Cross.API/obj/Debug/net10.0/ERP_Cross.API.AssemblyInfo.cs
+++ ERP_Cross.API/obj/Debug/net10.0/ERP_Cross.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP_Cross.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d324c2c59ea3093a397bc1a6b86decc733669f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaca184a4afc3f338eec69e4d9a37e4f4bab2cf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP_Cross.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP_Cross.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
